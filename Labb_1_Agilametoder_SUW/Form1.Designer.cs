ae752ac8b13ad257ef27fbb932d634dcb8519a13
unclebob79 <erik.moren@gmail.com>
2014-10-09 21:34:24 +0200

    first commit

---
+++ Labb_1_Agilametoder_SUW/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Labb_1_Agilametoder_SUW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.addToListButton = new System.Windows.Forms.Button();
+            this.showListButton = new System.Windows.Forms.Button();
+            this.searchListButton = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(136, 7);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(650, 472);
+            this.listBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(117, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 101);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(117, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // addToListButton
+            // 
+            this.addToListButton.Location = new System.Drawing.Point(12, 127);
+            this.addToListButton.Name = "addToListButton";
+            this.addToListButton.Size = new System.Drawing.Size(117, 44);
+            this.addToListButton.TabIndex = 4;
+            this.addToListButton.Text = "Lägg till";
+            this.addToListButton.UseVisualStyleBackColor = true;
+            this.addToListButton.Click += new System.EventHandler(this.addToListButton_Click);
+            // 
+            // showListButton
+            // 
+            this.showListButton.Location = new System.Drawing.Point(15, 223);
+            this.showListButton.Name = "showListButton";
+            this.showListButton.Size = new System.Drawing.Size(117, 44);
+            this.showListButton.TabIndex = 5;
+            this.showListButton.Text = "Visa lista";
+            this.showListButton.UseVisualStyleBackColor = true;
+            this.showListButton.Click += new System.EventHandler(this.showListButton_Click);
+            // 
+            // searchListButton
+            // 
+            this.searchListButton.Location = new System.Drawing.Point(13, 376);
+            this.searchListButton.Name = "searchListButton";
+            this.searchListButton.Size = new System.Drawing.Size(117, 93);
+            this.searchListButton.TabIndex = 6;
+            this.searchListButton.Text = "Sök";
+            this.searchListButton.UseVisualStyleBackColor = true;
+            this.searchListButton.Click += new System.EventHandler(this.searchListButton_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 350);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(117, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Namn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "E-postadress";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Telefonnummer";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Sökord";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(798, 481);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.searchListButton);
+            this.Controls.Add(this.showListButton);
+            this.Controls.Add(this.addToListButton);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button addToListButton;
+        private System.Windows.Forms.Button showListButton;
+        private System.Windows.Forms.Button searchListButton;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Labb_1_Agilametoder_SUW/Form1.cs
@@ -0,0 +1,126 @@
+﻿///Author: Erik Morén
+///Version: 1
+///Date: 2014-10-06
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Labb_1_Agilametoder_SUW
+{
+    public partial class Form1 : Form
+    {
+        List<String> Stringlist = new List<String>();
+        public Form1()
+        {
+            InitializeComponent();
+            InitList(); 
+        }
+
+        /// <summary>
+        /// Clears all input fields in the form.
+        /// </summary>
+        private void ClearAllInputFields()
+        {
+            textBox1.Clear(); 
+            textBox2.Clear(); 
+            textBox3.Clear();
+        }
+
+        /// <summary>
+        /// Initializes our list with start values.
+        /// </summary>
+        private void InitList()
+        {
+            Stringlist.Add("Namn     Mejl     Nummer");
+            Stringlist.Add("Bengt     benga55@gmail.com     0701234567");
+            Stringlist.Add("Peter     ptr@hotspot.com     0701234576");
+            Stringlist.Add("Lina     lina_w@gmail.com     0701239654");
+            Stringlist.Add("Ahmed     ahmed_ahmed@gmail.com     0707654321");
+            Stringlist.Add("Angelica     angelangelica@gmail.com     0701212125");
+            Stringlist.Add("Olof     olof_olofsson5@gmail.com     0702223336");
+            Stringlist.Add("Clara     stensson_c@gmail.com     0730456789");
+            Stringlist.Add("Stina     stinastinastinastina@hotmail.com     0700554412");
+            Stringlist.Add("Bertil     beritochbertil@telia.com     0700000258");
+        }
+
+        /// <summary>
+        /// Click evet for addToList button. Adds an element to our list.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void addToListButton_Click(object sender, EventArgs e)
+        {
+            Stringlist.Add(textBox1.Text + "     " + textBox2.Text + "     " + textBox3.Text);
+            ClearAllInputFields();
+        }
+
+        /// <summary>
+        /// Click event for showList button. Displays our list in our window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void showListButton_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("");
+            foreach (String str in Stringlist)
+            {
+                listBox1.Items.Add(str);
+            }
+
+            ClearAllInputFields();
+        }
+
+        /// <summary>
+        /// Click event for search button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        
+        private void searchListButton_Click(object sender, EventArgs e)
+        {
+            String searchStr = textBox4.Text.ToLower();
+            List<String> resultList = SearchList(searchStr);
+
+            listBox1.Items.Add("");
+            
+            if (resultList.Count > 0)
+            {
+                listBox1.Items.Add("Resultat av sökning:");
+                foreach (String str in resultList)
+                {
+                    listBox1.Items.Add(str);
+                }
+            }
+            else
+            {
+                listBox1.Items.Add("Sorry, hittade inget.");
+            }
+        }
+
+        /// <summary>
+        /// Searches through our list of strings for a match.
+        /// </summary>
+        /// <param name="searchStr">String with our search text</param>
+        /// <returns>List of Strings</returns>
+        private List<String> SearchList(String searchStr)
+        {
+            List<String> resultList = new List<String>();
+            foreach (String str in Stringlist)
+            {
+                if (str.ToLower().Contains(searchStr))
+                {
+                    resultList.Add(str);
+                }
+            }
+
+            return resultList;
+        }
+    }
+}
